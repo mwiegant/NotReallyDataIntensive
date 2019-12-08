c0e871417ce1c91985fde1d1b2033e56ce108ebc
Max Wiegant <mwiegant@nevada.unr.edu>
2019-12-08 11:23:40 -0800

    skeleton now in place for server

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /* File that defines the app and defines core libraries */
 
+const config = require('./bin/config.json');
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -11,8 +12,6 @@ var router = require('./lib/router');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,6 +20,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // setup routes here
 app.use('/', router);
-//app.use('/users', users);     // this line included for later reference
 
 module.exports = app;
