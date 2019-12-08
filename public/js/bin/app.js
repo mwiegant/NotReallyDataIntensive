f9ad32d624a1e81e5d4e26388fe5ada61c3d1516
Max Wiegant <mwiegant@nevada.unr.edu>
2019-12-08 13:15:20 -0800

    cleaned out the public folder

--- public/js/bin/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and core components
-var dndApp = angular.module('dndApp', ['ngRoute']);
-
-// may want to setup other things in this file, eventually?
